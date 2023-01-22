4c4eb49039bc119358e0945f2db999f56de10249
alexslort <104786558+alexslort@users.noreply.github.com>
2023-01-22 18:11:20 -0500

    Update EditContact.php

--- LAMPAPI/EditContact.php
+++ LAMPAPI/EditContact.php
@@ -1,11 +1,11 @@
 <?php
     $inData = getRequestInfo();
 
-    $userId = $inData["userId"];
-    $firstName = $inData["firstName"];
-    $lastName = $inData["lastName"];
-    $login = $inData["login"];
-    $password = $inData["password"];
+    $firstName = $inData["nfirstName"];
+    $lastName = $inData["nlastName"];
+    $phoneNumber = $inData["nphoneNumber"];
+    $email = $inData["nemail"];
+    $id = $inData["id"];
     
     $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
     if ($conn->connect_error)
@@ -16,8 +16,8 @@
     {
         // The command will look like UPDATE Contracts SET variable=? WHERE variable=?
         // leaving this blank for now as collaboration with team needs to happen to figure out how to do this
-        $stmt = $conn->prepare("UPDATE contacts SET FirstName=?, LastName=?, Email=?, PhoneNumber=? WHERE UserId=?");
-        $stmt->bind_param("sssss", $firstName, $lastName, $phoneNumber, $email, $userId);
+        $stmt = $conn->prepare("UPDATE contacts SET FirstName=?, LastName=?, Email=?, PhoneNumber=? WHERE ID=?");
+        $stmt->bind_param("sssss", $firstName, $lastName, $email, $phoneNumber, $id);
         $stmt->execute();
         $stmt->close();
         $conn->close();
@@ -26,7 +26,7 @@
 
     function getRequestInfo()
     {
-        return json_decode(file_put_contents('php://input'), true);
+        return json_decode(file_get_contents('php://input'), true);
     }
 
     function sendResultInfoAsJason($obj)
