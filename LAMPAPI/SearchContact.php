039e971a3a36ccf6ed9a29a3588c8fea772e5477
Kenny Dao <89817856+kenspacedao@users.noreply.github.com>
2023-01-30 21:08:55 -0500

    Commented SearchContact.php

--- LAMPAPI/SearchContact.php
+++ LAMPAPI/SearchContact.php
@@ -1,10 +1,11 @@
 <?php
-
+	//Retrieves the JSON POST with PHP
 	$inData = getRequestInfo();
 	
 	$searchResults = "";
 	$searchCount = 0;
 
+	//Connecting to the database and searches for contact
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 	if ($conn->connect_error) 
 	{
@@ -42,23 +43,27 @@
 		$conn->close();
 	}
 
+	//Receives JSON data as a string and decodes it
 	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
+	//Sends the result info as JSON
 	function sendResultInfoAsJson( $obj )
 	{
 		header('Content-type: application/json');
 		echo $obj;
 	}
 	
+	//Error handling
 	function returnWithError( $err )
 	{
 		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
 	
+	//Returns search results as a object
 	function returnWithInfo( $searchResults )
 	{
 		$retValue = '{"results":[' . $searchResults . '],"error":""}';
